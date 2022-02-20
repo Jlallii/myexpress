9d29545e4bc6bc8cc8f1d37842de161693679b09
Jlallii <t1laja03@students.oamk.fi>
2022-02-20 20:42:52 +0200

    express alustettu

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,25 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get ('/example',function(request,response)
+{
+  response.send("Hello");
+  console.log("I am example");
+});
 
+router.get ('/example/:name',function(request,response)
+{
+  response.send("Hello "+request.params.name);
+  console.log("I am example");
+});
+
+router.get ('/example2/:firstName/:lastName',function(request,response)
+{
+  response.send("Hello"+request.params.firstName+" "+request.params.lastName);
+  console.log("I am example");
+});
+router.post('/',function(request,response)
+{
+  response.send(request.body);
+});
 module.exports = router;
